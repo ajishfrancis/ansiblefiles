268c867fc93bf369fc5404f997ce1d527abcc16a
Ajish Chovallur <ajish.chovallur@atos.net>
2017-09-12 10:54:10 +0530

    updates

--- dnsscript.ps1
+++ dnsscript.ps1
@@ -2,7 +2,7 @@ $computer = get-content C:\Users\Administrator\Desktop\computername.txt
  $NICs = Get-WMIObject Win32_NetworkAdapterConfiguration -computername $computer |where{$_.IPEnabled -eq “TRUE”}
    Foreach($NIC in $NICs) {
 
-$DNSServers = “192.168.1.10",”192.168.1.2"
+$DNSServers = “192.168.1.131",”192.168.1.2"
   $NIC.SetDNSServerSearchOrder($DNSServers)
   $NIC.SetDynamicDNSRegistration(“TRUE”)
  }
